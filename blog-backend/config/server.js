252cec42399cba2b3edb2ada44da473cfcadd0fd
walidmahade <walid_mahade@outlook.com>
2021-03-06 19:49:47 +0600

    first commit

---
+++ blog-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd173c30c76f54bc49df0213777024f87'),
+    },
+  },
+});
---
+++ blog-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e222b80c-c5bc-414b-8b8d-32668d5768ee'
+};
